7c30c2eb373732200505df1f1c2421e99b2ebd8b
Ahmed Fasih <wuzzyview@gmail.com>
2015-11-23 11:51:39 -0500

    GTiff in module

--- cgiar.py
+++ cgiar.py
@@ -1,15 +1,31 @@
 import numpy as np
 import textureShading as tex
-filename = 'Data/east_0.1.tif'
+filename = 'Data/east_0.01.tif'
 
-data = tex.filenameToTexture(filename)
+generateData = True # if false, tries to load raw from disk
+saveRaw = True
+writeTif = True
+writePng = True
 
-if True:
+if generateData:
+    data = tex.filenameToTexture(filename)
+else:
+    data = np.load('texpy.npy')
+    print "Loaded data"
+
+if saveRaw and not generateData: # if data wasn't generated, it was loaded
     np.save('texpy.npy', data)
     print "Saved raw"
 
+# Re-apply mask
 data[tex.filenameToLandMask(filename)] = 0
 
-tex.dataToPng(data, 'modularized.png', [0.5, 99.9], [.55, .4]) 
-# also try [0.25, 0.2] for postPercentileScale. Or [0.55, 0.4]. Or [1, 1].
-print "Saved PNG"
+if writeTif:
+    tex.dataToGTiff16(data, filename, 'modularized.tif', [0.5, 99.9], [0.55, 0.4])
+    print "Saved TIF"
+
+if writePng:
+    tex.dataToPng16(data, 'modularized.png', [0.5, 99.9], [.55, .4]) 
+    # also try [0.25, 0.2] for postPercentileScale. Or [0.55, 0.4]. Or [1, 1].
+    print "Saved PNG"
+
--- textureShading.py
+++ textureShading.py
@@ -2,23 +2,27 @@ import numpy as np
 import gdal, gdalconst
 import numpy.fft as fft
 import scipy.fftpack as scifft
+from osgeo import osr
 
 nextpow2 = lambda v: 2 ** np.ceil(np.log2(v)).astype(int)
 
 
-def sciMoreReal(x, alpha):
+def sciMoreReal(x, alpha, verbose = True):
     Nyx = nextpow2(x.shape)
 
     fy = scifft.rfftfreq(Nyx[0])[:, np.newaxis].astype(x.dtype)
     fx = scifft.rfftfreq(Nyx[1])[np.newaxis, :].astype(x.dtype)
     H2 = np.sqrt(fx ** 2 + fy ** 2) ** alpha
+    if verbose: print "Generated filter"
 
     rfft2 = lambda x: scifft.rfft(scifft.rfft(x, Nyx[1], 1, True), Nyx[0], 0, True)
     irfft2 = lambda X: scifft.irfft(scifft.irfft(X, axis=0, overwrite_x=True), overwrite_x=True)
 
     xr = rfft2(x) * H2
+    if verbose: print "Completed frequency domain operations"
     H2 = [] # potentially trigger GC here to reclaim H2's memory
     xr = irfft2(xr)
+    if verbose: print "Back to spatial-domain"
 
     return xr[:x.shape[0], :x.shape[1]]
 
@@ -122,10 +126,50 @@ def dataToPercentileLimits(texture, percentiles, postPercentileScale):
     colorlimits = np.percentile(texture.ravel(), percentiles)
     return colorlimits * postPercentileScale
 
-def dataToPng(data, fname = 'pytex16.png', percentiles = [0.5, 99.9],
+def dataToPng16(data, fname = 'pytex16.png', percentiles = [0.5, 99.9],
         postPercentileScale = [1.0, 1.0]):
     import png
     limits = dataToPercentileLimits(data, percentiles, postPercentileScale)
     png.from_array(touint16(data, *limits), 'L').save(fname)
 
+# Adapted from EddyTheB, http://gis.stackexchange.com/a/57006/8623
+def GetGeoInfo(FileName):
+    SourceDS = gdal.Open(FileName, gdalconst.GA_ReadOnly)
+    NDV = SourceDS.GetRasterBand(1).GetNoDataValue()
+    xsize = SourceDS.RasterXSize
+    ysize = SourceDS.RasterYSize
+    GeoT = SourceDS.GetGeoTransform()
+    Projection = osr.SpatialReference()
+    Projection.ImportFromWkt(SourceDS.GetProjectionRef())
+    DataType = SourceDS.GetRasterBand(1).DataType
+    DataType = gdal.GetDataTypeName(DataType)
+    return NDV, xsize, ysize, GeoT, Projection, DataType
+
+def CreateGeoTiff(Name, Array, driver, NDV, 
+                  xsize, ysize, GeoT, Projection, DataType):
+    if DataType == 'Float32':
+        DataType = gdal.GDT_Float32
+    NewFileName = Name+'.tif'
+    # Set nans to the original No Data Value
+    if NDV is not None: Array[np.isnan(Array)] = NDV
+    # Set up the dataset
+    DataSet = driver.Create( NewFileName, xsize, ysize, 1, DataType )
+            # the '1' is for band 1.
+    DataSet.SetGeoTransform(GeoT)
+    DataSet.SetProjection( Projection.ExportToWkt() )
+    # Write the array
+    DataSet.GetRasterBand(1).WriteArray( Array )
+    if NDV is not None: DataSet.GetRasterBand(1).SetNoDataValue(NDV)
+    return NewFileName
+
+def dataToGTiff16(data, infile, outfile = 'pytex16.tiff', percentiles = [0.5, 99.9],
+        postPercentileScale = [1.0, 1.0]):
+    driver = gdal.GetDriverByName('GTiff')
+    NDV, xsize, ysize, GeoT, Projection, DataType = GetGeoInfo(infile)
+
+    limits = dataToPercentileLimits(data, percentiles, postPercentileScale)
+    data = touint16(data, *limits)
+    
+    CreateGeoTiff(outfile, data, driver, NDV, xsize, ysize, GeoT, Projection,
+            gdalconst.GDT_UInt16)
 
