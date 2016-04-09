07b0530bdc7b1c1423e7712294deae156395f268
Ahmed Fasih <wuzzyview@gmail.com>
2016-04-09 11:34:11 -0400

    Hankel generator minor tweaks

--- genHyperHankel.py
+++ genHyperHankel.py
@@ -8,7 +8,6 @@ sol = sympy.integrate(rS ** aS * rS * sympy.besselj(0, 2 * sympy.pi * vS * rS),
                       (rS, 0, fmax)) * 2 * sympy.pi
 
 alpha = 0.5
-N = 20
 
 class EvalSol(object):
     def __init__(self, alpha):
@@ -18,7 +17,8 @@ class EvalSol(object):
             Ns), dtype=np.float)
 evaler = EvalSol(alpha)
 
-radii = np.arange(0, 256*1024+1e-3, 1/32.0)
+radii = np.arange(0, 128*1024+1e-3, 1/32.0)
+# radii = np.arange(0, 2*1024+1e-3, 1/32.0)
 # radii = np.arange(0, 0.9, 1/32.0)
 
 from multiprocessing import Pool
